b63c6061a4dd95700df3c81f954d41ec16934cc7
Your Name <Oluwadamilolaayeni321@gmail.com>
2025-07-17 14:55:47 +0100

    update on footer

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <footer style={{ backgroundColor: '#333', color: 'white', textAlign: 'center', padding: '10px' }}>
-      <p>&copy; 2025 My Favorite Cities. All rights reserved.</p>
+      <p>© 2023 City Lovers</p>
     </footer>
   );
 }
