35fd012f4ac9e5b2a9349565435b86322806b09b
Your Name <Oluwadamilolaayeni321@gmail.com>
2025-07-19 10:42:09 +0100

    Duplicate alx-react-app to alx-react-app-props for props-focused work

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -10,15 +10,12 @@ import UserProfile from './components/UserProfile';
 import ProfilePage from './ProfilePage';
 import UserContext from './UserContext';
 
-
 function App() {
   const [count, setCount] = useState(0)
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
-
   return (
     <>
-  
       <div>
         <a href="https://vite.dev" target="_blank" rel="noopener noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -36,10 +33,10 @@ function App() {
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography." />
       <Footer />
 
-    <UserContext.Provider value={userData}>
-      <ProfilePage />
-    </UserContext.Provider>
-      
+      <UserContext.Provider value={userData}>
+        <ProfilePage />
+      </UserContext.Provider>
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
