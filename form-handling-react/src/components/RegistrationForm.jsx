584157b61af9050968b1d25582c9ea8398c663fc
Your Name <Oluwadamilolaayeni321@gmail.com>
2025-08-21 16:26:59 +0100

    Implementing Form Handling in React with Controlled Components and Formik

---
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { useState } from "react";
+
+const RegistrationForm =() => {
+    const [username, setUsername] = useState ("");
+    const [email, setEmail] = useState ("");
+    const [password, setPassword] = useState ("");
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+        if (!username|| !email|| !password) {
+            alert("All fields are required!")
+            return;
+        }
+    console.log({ username, email, password });    
+
+
+        setUsername("")
+        setEmail("")
+        setPassword("")
+};
+
+return (
+    <form onSubmit={handleSubmit}>
+        <input
+    type="text"
+    placeholder="username"
+    value={username}
+    onChange={(e) => 
+        setUsername(e.target.value)}
+        />
+
+        <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) =>
+            setEmail(e.target.value)}
+            /> 
+
+        <input 
+        type="password"
+        placeholder="Password"
+        value= {password}
+        onChange={(e)=> 
+            setPassword(e.target.value)}
+            />
+            <button type="submit">Register</button>
+    </form>
+);
+        };
+
+        export default RegistrationForm;
+
+
+
+
+
+
+
+9
+
