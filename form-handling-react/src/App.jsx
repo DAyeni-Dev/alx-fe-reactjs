838558c9327c6f35214512dbcfa40ca35e05f990
Your Name <Oluwadamilolaayeni321@gmail.com>
2025-08-21 16:30:45 +0100

    updated app.jsx

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
-import RegistrationForm from './components/RegistrationForm';
-import './App.css'
+import React from "react";
+import RegistrationForm from "./components/RegistrationForm";
+import FormikForm from "./components/FormikForm";
+import "./App.css";
 
 function App() {
   return (
     <div>
       <h1>User Registration</h1>
       <RegistrationForm />
+
+      <h1>User Registration (Formik)</h1>
+      <FormikForm />
     </div>
   );
 }
- export default App;
-  
+
+export default App;
