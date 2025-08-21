e9c3e11b531be363b9a427fa485eddfd1fa02b0e
Your Name <Oluwadamilolaayeni321@gmail.com>
2025-08-21 14:00:23 +0100

    Update recipe form

--- recipe-sharing-platform/src/components/AddRecipeForm.jsx
+++ recipe-sharing-platform/src/components/AddRecipeForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 
+
 export default function AddRecipeForm({ onAddRecipe }) {
   const [title, setTitle] = useState("");
   const [ingredients, setIngredients] = useState("");
