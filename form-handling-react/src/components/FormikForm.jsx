42459a6e810db434c59e1988ee51f07f3cdacdcf
Your Name <Oluwadamilolaayeni321@gmail.com>
2025-08-21 16:27:35 +0100

    Implementing Form Handling in React with Controlled Components and Formik

---
+++ form-handling-react/src/components/FormikForm.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+const FormikForm = () => {
+  const initialValues = {
+    username: "",
+    email: "",
+    password: "",
+  };
+
+  const validationSchema = Yup.object({
+    username: Yup.string().required("Username is required"),
+    email: Yup.string().email("Invalid email format").required("Email is required"),
+    password: Yup.string().required("Password is required"),
+  });
+
+  const handleSubmit = (values, { resetForm }) => {
+    console.log(values);
+    resetForm();
+  };
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={handleSubmit}
+    >
+      <Form>
+        <div>
+          <Field type="text" name="username" placeholder="Username" />
+          <ErrorMessage name="username" component="div" style={{ color: "red" }} />
+        </div>
+
+        <div>
+          <Field type="email" name="email" placeholder="Email" />
+          <ErrorMessage name="email" component="div" style={{ color: "red" }} />
+        </div>
+
+        <div>
+          <Field type="password" name="password" placeholder="Password" />
+          <ErrorMessage name="password" component="div" style={{ color: "red" }} />
+        </div>
+
+        <button type="submit">Register</button>
+      </Form>
+    </Formik>
+  );
+};
+
+export default FormikForm;
