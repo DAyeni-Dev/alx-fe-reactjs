2a614e9b34280cf17684fe0f90c6b0c2b8afb13b
Your Name <Oluwadamilolaayeni321@gmail.com>
2025-08-22 05:34:36 +0100

    Implemented nested routes

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -3,8 +3,6 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-d
 import Home from "./components/Home";
 import About from "./components/About";
 import Profile from "./components/Profile";
-import ProfileDetails from "./components/ProfileDetails";
-import ProfileSettings from "./components/ProfileSettings";
 import BlogPost from "./components/BlogPost";
 import ProtectedRoute from "./components/ProtectedRoute";
 
@@ -15,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
 
+       
         <Route
           path="/profile/*"
           element={
@@ -22,13 +21,12 @@ function App() {
               <Profile />
             </ProtectedRoute>
           }
-        >
-          <Route path="details" element={<ProfileDetails />} />
-          <Route path="settings" element={<ProfileSettings />} />
-        </Route>
+        />
 
+    
         <Route path="/blog/:id" element={<BlogPost />} />
 
+        
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,17 +1,26 @@
-import React from "react"
-import { Outlet, Link } from "react-router-dom"
+import React from "react";
+import { Link, Routes, Route } from "react-router-dom";
+import ProfileDetails from "./ProfileDetails";
+import ProfileSettings from "./ProfileSettings";
 
-function Profile () {
-    return(
-        <div>
-        <h1> Profile Page</h1>
-        <nav>
-            <Link to="details">Details</Link>
-            <Link to="settings">Settings</Link>
-        </nav>
-        <Outlet/>
+function Profile() {
+  return (
+    <div>
+      <h2>Profile Page</h2>
+
+      <nav>
+        <ul>
+          <li><Link to="details">Profile Details</Link></li>
+          <li><Link to="settings">Profile Settings</Link></li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="details" element={<ProfileDetails />} />
+        <Route path="settings" element={<ProfileSettings />} />
+      </Routes>
     </div>
-    );
+  );
 }
 
-export default Profile
+export default Profile;
