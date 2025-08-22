dace72bff2cbd1fe4e0fa41be4f2a75f338565d9
Your Name <Oluwadamilolaayeni321@gmail.com>
2025-08-22 03:25:09 +0100

    created data fetching components

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -1,17 +1,27 @@
-// src/components/PostsComponent.jsx
+import React from "react";
 import { useQuery } from "@tanstack/react-query";
 
+const fetchPosts = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  if (!res.ok) {
+    throw new Error("Network response was not ok");
+  }
+  return res.json();
+};
+
 function PostsComponent() {
-  const { data: posts, isLoading, isError, error, refetch } = useQuery({
+  const {
+    data: posts,
+    isLoading,
+    isError,
+    error,
+    refetch,
+  } = useQuery({
     queryKey: ["posts"],
-    queryFn: async () => {
-      const response = await fetch("https://jsonplaceholder.typicode.com/posts");
-      if (!response.ok) throw new Error("Network response was not ok");
-      return response.json();
-    },
-    staleTime: 1000 * 60 * 5,
-    cacheTime: 1000 * 60 * 10,
-    refetchOnWindowFocus: false,
+    queryFn: fetchPosts,
+    staleTime: 5 * 60 * 1000,
+    cacheTime: 10 * 60 * 1000,
+    refetchOnWindowFocus: true,
     keepPreviousData: true,
   });
 
